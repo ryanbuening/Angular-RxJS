5a795a665b0c5ea342e78910b70e8be1c74a2716
Ryan Buening <ryan.buening@celinainsurance.com>
2020-07-29 13:45:07 -0400

    completed Module 5

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,40 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
-import { Subscription, Observable, of, EMPTY } from 'rxjs';
+import { EMPTY } from 'rxjs';
 
-import { Product } from './product';
 import { ProductService } from './product.service';
 import { catchError } from 'rxjs/operators';
 
 @Component({
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit {
-  pageTitle = 'Product List';
-  errorMessage = '';
-  categories;
+export class ProductListComponent {
+    pageTitle = 'Product List';
+    errorMessage = '';
+    categories;
 
-  products$: Observable<Product[]>;
-  sub: Subscription;
-
-  constructor(private productService: ProductService) { }
-
-  ngOnInit(): void {
-    this.products$ = this.productService.getProducts()
+    products$ = this.productService.products$
         .pipe(
             catchError(err => {
                 this.errorMessage = err;
                 return EMPTY;
             })
         );
-  }
 
-  onAdd(): void {
-    console.log('Not yet implemented');
-  }
+    constructor(private productService: ProductService) { }
 
-  onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
-  }
+    onAdd(): void {
+        console.log('Not yet implemented');
+    }
+
+    onSelected(categoryId: string): void {
+        console.log('Not yet implemented');
+    }
 }
--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -9,50 +9,49 @@ import { Supplier } from '../suppliers/supplier';
 import { SupplierService } from '../suppliers/supplier.service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ProductService {
-  private productsUrl = 'api/products';
-  private suppliersUrl = this.supplierService.suppliersUrl;
+    private productsUrl = 'api/products';
+    private suppliersUrl = this.supplierService.suppliersUrl;
 
-  constructor(private http: HttpClient,
-              private supplierService: SupplierService) { }
+    products$ = this.http.get<Product[]>(this.productsUrl)
+        .pipe(
+            tap(data => console.log('Products: ', JSON.stringify(data))),
+            catchError(this.handleError)
+        );
 
-  getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.productsUrl)
-      .pipe(
-        tap(data => console.log('Products: ', JSON.stringify(data))),
-        catchError(this.handleError)
-      );
-  }
+    constructor(
+        private http: HttpClient,
+        private supplierService: SupplierService) { }
 
-  private fakeProduct(): Product {
-    return {
-      id: 42,
-      productName: 'Another One',
-      productCode: 'TBX-0042',
-      description: 'Our new product',
-      price: 8.9,
-      categoryId: 3,
-      // category: 'Toolbox',
-      quantityInStock: 30
-    };
-  }
-
-  private handleError(err: any): Observable<never> {
-    // in a real world app, we may send the server to some remote logging infrastructure
-    // instead of just logging it to the console
-    let errorMessage: string;
-    if (err.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      errorMessage = `An error occurred: ${err.error.message}`;
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
+    private fakeProduct(): Product {
+        return {
+            id: 42,
+            productName: 'Another One',
+            productCode: 'TBX-0042',
+            description: 'Our new product',
+            price: 8.9,
+            categoryId: 3,
+            // category: 'Toolbox',
+            quantityInStock: 30
+        };
     }
-    console.error(err);
-    return throwError(errorMessage);
-  }
+
+    private handleError(err: any): Observable<never> {
+        // in a real world app, we may send the server to some remote logging infrastructure
+        // instead of just logging it to the console
+        let errorMessage: string;
+        if (err.error instanceof ErrorEvent) {
+            // A client-side or network error occurred. Handle it accordingly.
+            errorMessage = `An error occurred: ${err.error.message}`;
+        } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong,
+            errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
+        }
+        console.error(err);
+        return throwError(errorMessage);
+    }
 
 }
