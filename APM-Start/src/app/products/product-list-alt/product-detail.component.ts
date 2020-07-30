5d483720f64b11ab79bd10515ce1f299940a7294
Ryan Buening <ryan.buening@celinainsurance.com>
2020-07-30 16:54:38 -0400

    after module 9 Reacting to Selection: Data Stream

--- APM-Start/src/app/products/product-list-alt/product-detail.component.ts
+++ APM-Start/src/app/products/product-list-alt/product-detail.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 import { ProductService } from '../product.service';
+import { EMPTY } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
-  selector: 'pm-product-detail',
-  templateUrl: './product-detail.component.html'
+    selector: 'pm-product-detail',
+    templateUrl: './product-detail.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductDetailComponent {
-  pageTitle = 'Product Detail';
-  errorMessage = '';
-  product;
+    pageTitle = 'Product Detail';
+    errorMessage = '';
 
-  constructor(private productService: ProductService) { }
+    product$ = this.productService.selectedProduct$
+        .pipe(
+            catchError(err => {
+                this.errorMessage = err;
+                return EMPTY;
+            })
+        );
+
+    constructor(private productService: ProductService) { }
 
 }
