ffce832ab2b6e7e04d77ed18deaff071b5dd92c8
Ryan Buening <ryan.buening@celinainsurance.com>
2020-07-30 15:46:57 -0400

    completed module 8

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,9 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 
-import { EMPTY } from 'rxjs';
+import { EMPTY, Subject, combineLatest, BehaviorSubject } from 'rxjs';
 
 import { ProductService } from './product.service';
-import { catchError } from 'rxjs/operators';
+import { catchError, map, startWith } from 'rxjs/operators';
+import { ProductCategoryService } from '../product-categories/product-category.service';
 
 @Component({
     templateUrl: './product-list.component.html',
@@ -13,9 +14,27 @@ import { catchError } from 'rxjs/operators';
 export class ProductListComponent {
     pageTitle = 'Product List';
     errorMessage = '';
-    categories;
+    // selectedCategoryId = 1;
 
-    products$ = this.productService.productsWithCategory$
+    private categorySelectedSubject = new BehaviorSubject<number>(0);
+    categorySelectedAction$ = this.categorySelectedSubject.asObservable();
+
+    products$ = combineLatest([
+        this.productService.productsWithCategory$,
+        this.categorySelectedAction$
+    ])
+        .pipe(
+            map(([products, selectedCategoryId]) =>
+                products.filter(product =>
+                    selectedCategoryId ? product.categoryId === selectedCategoryId : true
+                )),
+            catchError(err => {
+                this.errorMessage = err;
+                return EMPTY;
+            })
+        );
+
+    categories$ = this.productCategoryService.productCategories$
         .pipe(
             catchError(err => {
                 this.errorMessage = err;
@@ -23,13 +42,24 @@ export class ProductListComponent {
             })
         );
 
-    constructor(private productService: ProductService) { }
+    // productsSimpleFilter$ = this.productService.productsWithCategory$
+    //     .pipe(
+    //         map(products =>
+    //             products.filter(product =>
+    //                 this.selectedCategoryId ? product.categoryId === this.selectedCategoryId : true
+    //             ))
+    //     );
+
+    constructor(
+        private productService: ProductService,
+        private productCategoryService: ProductCategoryService) { }
 
     onAdd(): void {
         console.log('Not yet implemented');
     }
 
     onSelected(categoryId: string): void {
-        console.log('Not yet implemented');
+        // this.selectedCategoryId = +categoryId;
+        this.categorySelectedSubject.next(+categoryId);
     }
 }
